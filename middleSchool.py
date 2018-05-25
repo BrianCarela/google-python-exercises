22ef62f2e7d29b594c722e7494dbd9e0c6851cbd
Brian Carela <BrianCarela@gmail.com>
2018-05-25 15:00:29 -0400

    added a fun file

--- middleSchool.py
+++ middleSchool.py
@@ -1,2 +1,16 @@
 import sys
 
+def main():
+    u_gay = raw_input("Are you gay? (yes/no)")
+    if(u_gay == "no"):
+        trick = raw_input("Does your mom know you're gay? (yes/no)")
+        if(trick == "no"):
+            print("LOL ur gay bro")
+        else:
+            print("... Bet")
+    elif(u_gay == "yes"):
+        print("Facts, I respect it")
+    else: main()
+
+if __name__ == '__main__':
+  main()
